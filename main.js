a20a449fb9c0b64cf30dc0999954bd3506698985
migueel15 <miguelangeldorado10@gmail.com>
2025-05-18 15:53:50 +0200

    initial test

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import express from "express";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json({ status: 200, message: "Hello World" });
+});
 app.get("/auth", (req, res) => {
   res.json({ status: 200, code: "este es tu codigo" });
 });
